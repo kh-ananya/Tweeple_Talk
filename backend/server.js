9f65191dea84f6a7afbc816ff7a4f2c378083066
Ananya <kh.ananya.025@gmail.com>
2024-05-02 09:15:14 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ cloudinary.config({
 
 const app = express()
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "5mb"}))
 app.use(express.urlencoded({extended : true}))
--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -161,7 +161,7 @@ const Post = ({ post }) => {
 					<div className='flex justify-between mt-3'>
 						<div className='flex gap-4 items-center w-2/3 justify-between'>
 							<div
-								className='flex gap-1 items-center cursor-pointer group'
+								className='flex gap-1 ml-36 items-center cursor-pointer group'
 								onClick={() => document.getElementById("comments_modal" + post._id).showModal()}
 							>
 								<FaRegComment className='w-4 h-4  text-slate-500 group-hover:text-sky-400' />
@@ -219,11 +219,7 @@ const Post = ({ post }) => {
 									<button className='outline-none'>close</button>
 								</form>
 							</dialog>
-							<div className='flex gap-1 items-center group cursor-pointer'>
-								<BiRepost className='w-6 h-6  text-slate-500 group-hover:text-green-500' />
-								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
-							</div>
-							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
+							<div className='flex gap-1 items-end text-end group cursor-pointer' onClick={handleLikePost}>
 								{isLiking && <LoadingSpinner size='sm' />}
 								{!isLiked && !isLiking && (
 									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
@@ -240,9 +236,6 @@ const Post = ({ post }) => {
 									{post.likes.length}
 								</span>
 							</div>
-						</div>
-						<div className='flex w-1/3 justify-end gap-2 items-center'>
-							<FaRegBookmark className='w-4 h-4 text-slate-500 cursor-pointer' />
 						</div>
 					</div>
 				</div>
